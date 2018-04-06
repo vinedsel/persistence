e2db1e72932aaa76449f80270316dc7b7956f0fb
Raymond Gregory <vinedsel@gmail.com>
2018-04-05 19:28:06 -0500

    Added node modules with sqlite3.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require("express");
-const sqlite = require("sqlite3").verbose();
+const sqlite3 = require("sqlite3").verbose();
 
 const db = new sqlite3.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");

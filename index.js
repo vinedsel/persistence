fa3ed1ec6dfe1a4e3122202cbef7ea83ad867f68
Raymond Gregory <vinedsel@gmail.com>
2018-04-07 18:17:36 -0500

    Updated index.js, Replaced chinook db file with one that is actually populated thereby replacing the suspiciously empty file.

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
+
+const app = express();
 
 const db = new sqlite3.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");
+
+
+const query = `SELECT * from Artist LIMIT 100`;
+
+db.each(query, (err, row) => {
+ if (err) throw err;
+ console.log(row);
+});
+
+
+app.listen( process.env.PORT || 3000, ()=>{
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
+});
